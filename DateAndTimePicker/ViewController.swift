77d25fb9fe7b08426c257ecdf0a6a60c6affc501
ogullcankara <ogullcankara@gmail.com>
2022-08-03 00:53:12 +0300

    First Commit

--- DateAndTimePicker/ViewController.swift
+++ DateAndTimePicker/ViewController.swift
@@ -9,11 +9,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textFieldDate: UITextField!
+    
+    @IBOutlet weak var textFieldTime: UITextField!
+    
+    var datePicker:UIDatePicker?
+    var timePicker:UIDatePicker?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        
+        timePicker = UIDatePicker()
+        timePicker?.datePickerMode = .time
+        textFieldTime.inputView = timePicker
+        
+        //saat eski görünüm için
+        /*if #available(iOS 13.4, *) {
+            timePicker?.preferredDatePickerStyle = .wheels
+        } */
+        
+        let dokunmaAlgilama = UITapGestureRecognizer(target: self, action: #selector(self.dokunmaAlgilama))
+        
+        view.addGestureRecognizer(dokunmaAlgilama)
+        
+        //tarih eski görünüm için
+        if #available(iOS 13.4, *) {
+            datePicker?.preferredDatePickerStyle = .wheels
+        }
+        
+        textFieldDate.inputView = datePicker
+        
+        datePicker?.addTarget(self, action: #selector(self.tarihGoster(datePicker:)), for: .valueChanged)
+        
+        timePicker?.addTarget(self, action: #selector(self.saatGoster(timePicker:)), for: .valueChanged)
+        
+    }
+    
+    @objc func tarihGoster(datePicker:UIDatePicker){
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        let alinanTarih = dateFormatter.string(from: datePicker.date)
+        
+        print(alinanTarih)
+        textFieldDate.text = alinanTarih
+        
+    }
+    
+    @objc func saatGoster(timePicker:UIDatePicker){
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "hh:mm:ss"
+        
+        let alinanSaat = dateFormatter.string(from: timePicker.date)
+        textFieldTime.text = alinanSaat
+        
+    }
+    
+    @objc func dokunmaAlgilama(){
+        view.endEditing(true)
     }
 
-
+    
+    
 }
 
